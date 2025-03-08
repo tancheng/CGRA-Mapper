1a940903ae92a619fd76c51947612225dd388f2f
HardyVon <153832933+HardyVon@users.noreply.github.com>
2025-03-08 16:05:19 +0800

    [Update] verify.sh
    Bug fixed.

--- test/dvfs/verify.sh
+++ test/dvfs/verify.sh
@@ -1,4 +1,4 @@
-mapping_success=$(grep -o '[Mapping Success]' trace.log | wc -l)
+mapping_success=$(grep -o '\[Mapping Success\]' trace.log | wc -l)
 dvfs_enabled=$(grep -o 'tile average DVFS frequency level' trace.log | wc -l)
 dvfs_no_effect=$(grep -o 'tile average DVFS frequency level: 100%' trace.log | wc -l)
 if [ "$mapping_success" -eq 1 ] && [ "$dvfs_enabled" -eq 1 ] && [ "$dvfs_no_effect" -eq 0 ]; then
