5cfd3144554f6a9f4a9d0f0c4856e0934e85e41f
HardyVon <153832933+HardyVon@users.noreply.github.com>
2025-03-13 14:15:37 +0800

    Update compile.sh
    Disable llvm-dis for Github workflow does not support this command.

--- test/inter_edge/compile.sh
+++ test/inter_edge/compile.sh
@@ -1,4 +1,4 @@
 clang-12 -emit-llvm -fno-unroll-loops -O3 -o inter_edge_test.bc -c inter_edge_test.cpp
-llvm-dis inter_edge_test.bc -o inter_edge_test.ll
+# llvm-dis inter_edge_test.bc -o inter_edge_test.ll
 # opt --loop-unroll --unroll-count=4 kernel.bc -o kernel_unroll.bc
 # llvm-dis kernel_unroll.bc -o kernel_unroll.ll
