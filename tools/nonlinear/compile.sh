0d2b07caf6bd8b4279c16c03403180470278361e
MeowMJ <2998673969@qq.com>
2025-04-14 04:05:32 +0000

    [FIXED]update cmake.yml to avoid selecting wrong functions when testing fusion via Nonlinear kernel

---
+++ tools/nonlinear/compile.sh
@@ -0,0 +1,2 @@
+clang-12 -emit-llvm -O3 -fno-unroll-loops -fno-vectorize -o nonlinear_test.bc -c nonlinear_test.cpp
+# sed -i 's/\("testingOpcodeOffset"[[:space:]]*:[[:space:]]*\)0,/\12,/' param.json
