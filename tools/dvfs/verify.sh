626461fb264fd61d73c8d28243fbefc935a5843c
MeowMJ <2998673969@qq.com>
2025-04-21 02:31:49 +0000

    [DEBUG] dvfs

--- tools/dvfs/verify.sh
+++ tools/dvfs/verify.sh
@@ -2,11 +2,11 @@
 mapping_success=$(grep -o '\[Mapping Success\]' trace.log | wc -l)
 dvfs_enabled=$(grep -o 'tile average DVFS frequency level' trace.log | wc -l)
 dvfs_no_effect=$(grep -o 'tile average DVFS frequency level: 100%' trace.log | wc -l)
-if [ "$mapping_success" -eq 0 ] && [ "$dvfs_enabled" -eq 1 ] && [ "$dvfs_no_effect" -eq 0 ]; then
+if [ "$mapping_success" -eq 1 ] && [ "$dvfs_enabled" -eq 1 ] && [ "$dvfs_no_effect" -eq 0 ]; then
     echo "DVFS Test Pass!"
 else
     error_message="DVFS Test Fail! "
-    if [ "$mapping_success" -ne 0 ]; then
+    if [ "$mapping_success" -ne 1 ]; then
         error_message+="mapping_success is not equal to 1. "
     fi
     if [ "$dvfs_enabled" -ne 1 ]; then
