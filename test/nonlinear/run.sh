ef67caeb5ad447cef6a50fb6c6823261c5ffdca8
Yuqi Sun <791079835@qq.com>
2025-05-11 13:56:19 +0800

    Test github build.

--- test/nonlinear/run.sh
+++ test/nonlinear/run.sh
@@ -1,4 +1,4 @@
-#opt-12 -load ../../build/src/libmapperPass.so -mapperPass nonlinear_test.bc
-opt-12 -load ./libmapperPass.so -mapperPass nonlinear_test.bc
+opt-12 -load ../../build/src/libmapperPass.so -mapperPass nonlinear_test.bc
+#opt-12 -load ./libmapperPass.so -mapperPass nonlinear_test.bc
 # dot -Tpng _Z6kernelPfS_.dot -o kernel.png
 # clang-12 -O3 -emit-llvm -fno-unroll-loops -fno-vectorize nonlinear_test.cpp -S -o nonlinear_test.ll
