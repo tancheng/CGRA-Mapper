871b78a59a6ac28f31dea5a59bffea3692f51231
root <root@LAPTOP-VPLF7SOH.localdomain>
2025-07-26 08:19:21 +0800

    Fix error2

--- src/CGRANode.cpp
+++ src/CGRANode.cpp
@@ -43,15 +43,15 @@ CGRANode::CGRANode(int t_id, int t_x, int t_y) {
   m_regs_timing = NULL;
 
   // used for parameterizable CGRA functional units
-  m_canAdd   = true;
-  m_canMul   = true;
-  m_canShift = true;
-  m_canPhi   = true;
-  m_canSel   = true;
-  m_canCmp   = true;
-  m_canMAC   = true;
-  m_canLogic = true;
-  m_canBr    = true;
+  m_canAdd    = true;
+  m_canMul    = true;
+  m_canShift  = true;
+  m_canPhi    = true;
+  m_canSel    = true;
+  m_canCmp    = true;
+  m_canMAC    = true;
+  m_canLogic  = true;
+  m_canBr     = true;
   m_canReturn = true;
 
   // supportDVFS should be leveraged with the optLatency (i.e., multi-
